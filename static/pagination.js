086027995c97a43d703e1d67095aa767366a4eeb
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-02-18 16:47:52 -0500

    cosmetic changes

--- static/pagination.js
+++ static/pagination.js
@@ -3,7 +3,7 @@ $(document).ready( function () {
     // define a user model and give it some simple method
     var User = Backbone.Model.extend({
         fullInfo: function () {
-            return this.get('name') + "///" + this.get('age');
+            return this.get('name') + " /// " + this.get('age');
         },
     });
 
@@ -54,7 +54,8 @@ $(document).ready( function () {
         el : $('#users'),  // dom element displaying users
         events: { 'click button#next' : 'nextPage' },
         initialize: function() { 
-            // make this refer to UsersView in the methods below
+            // make this refer to UsersView in the methods below. Might not
+            // be necessary anymore
             _.bindAll(this, 'appendUser', 'render', 'nextPage');
             this.collection = new Users();
             this.collection.bind('next', this.nextPage);
@@ -67,13 +68,14 @@ $(document).ready( function () {
             // conditionally
             if (this.collection.hasCursor())
                 html += "<button id='next'>Next page</button>";
-
             $(this.el).html(html);
+
             var self = this;
             _(this.collection.models).each(function (user) {
                 self.appendUser(user);
             }, this);
         },
+
         appendUser: function(user){
             $('ul', this.el).append("<li>" + user.fullInfo() + "</li>");
         },
